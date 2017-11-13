2a37531d60880955297271b63fde0189be1f66a8
Jonathan <jszablevski@protonmail.com>
2017-11-13 15:53:36 -0200

    Use extract-text-webpack-plugin to bundle style to a css file

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack');
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: ['babel-polyfill', './src/index.js'],
@@ -11,8 +12,14 @@ module.exports = {
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
-      { test: /\.scss$/, exclude: /node_modules/, use: ['style-loader', 'css-loader', 'sass-loader'] },
+      { test: /\.scss$/, exclude: /node_modules/, use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: ['css-loader', 'sass-loader']
+      }) },
       { test: /\.(png|svg|jpg|gif)$/, exclude: /node_modules/, loader: 'file-loader' }
     ]
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css')
+  ]
 };
